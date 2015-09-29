505b059856d2e436f063ce9865fb3bd25aef09df
Michael Lindon <nitro9090@yahoo.com>
2015-09-29 08:56:47 -0700

    config upload

--- private/config/siteConfig.php
+++ private/config/siteConfig.php
@@ -1,3 +1,9 @@
 <?php
+//The site's configuration file.  It holds any site wide variables and common use variables (aka global static variables). 
+//Like the site's name and a variable for the root path
 
 $site_name = "Site Name";
+
+// sets a variable name to the site's root path and locaion of the common folder (used for common PHP, css, javascript files when serving a page)
+$rootPath = $_SERVER['DOCUMENT_ROOT'];
+$commonFolder = $rootPath . "/public/common/";
